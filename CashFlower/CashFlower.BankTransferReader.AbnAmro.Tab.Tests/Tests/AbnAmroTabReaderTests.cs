3b88a3999bcc92841728a1f9172719caabb84b89
ivoraedts <ivoraedts@yahoo.co.uk>
2017-04-13 12:53:10 +0200

    Started work on the ABN AMRO Tab file reader
    Right now it does nothing except for checking if the file exist and
    throwing a Not Implemented exception on the first line encountered. So
    this could currently be any file reader.

---
+++ CashFlower/CashFlower.BankTransferReader.AbnAmro.Tab.Tests/Tests/AbnAmroTabReaderTests.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace CashFlower.BankTransferReader.AbnAmro.Tab.Tests.Tests
+{
+    [TestFixture]
+    public class AbnAmroTabReaderTests
+    {
+        [Test]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void GivenUnexistingFile_ThrowsException()
+        {
+            new AbnAmroTabReader(@"Some//Unexisting//Location").GetBankTransfers();
+        }
+
+        [Test]
+        public void GivenEmptyFile_EmptyListIsReturned()
+        {
+            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\EmptyFile.tab");
+            var result = new AbnAmroTabReader(filename).GetBankTransfers();
+            Assert.IsEmpty(result);
+        }
+    }
+}
---
+++ CashFlower/CashFlower.BankTransferReader.AbnAmro.Tab/AbnAmroTabReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CashFlower.Contracts;
+
+namespace CashFlower.BankTransferReader.AbnAmro.Tab
+{
+    public class AbnAmroTabReader: IBankTransferReader
+    {
+        private readonly string _fullFilename ;
+        public AbnAmroTabReader(string fullFilename)
+        {
+            _fullFilename = fullFilename;
+        }
+
+        public List<BankTransferLine> GetBankTransfers()
+        {
+            if (!File.Exists(_fullFilename))
+                throw new FileNotFoundException(_fullFilename);
+
+            var result = new List<BankTransferLine>();
+            using (var file = new StreamReader(_fullFilename))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+            return result;
+        }
+    }
+}
