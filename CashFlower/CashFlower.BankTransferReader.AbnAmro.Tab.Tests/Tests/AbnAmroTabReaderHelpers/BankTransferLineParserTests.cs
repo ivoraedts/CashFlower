2c0d84917c2cfffb2f2cfe2d96fdf9ce5435ae3b
ivoraedts <ivoraedts@yahoo.co.uk>
2017-04-18 23:19:06 +0200

    Refactored unit tests. Got rid of unneeded resources.

--- CashFlower/CashFlower.BankTransferReader.AbnAmro.Tab.Tests/Tests/AbnAmroTabReaderHelpers/BankTransferLineParserTests.cs
+++ CashFlower/CashFlower.BankTransferReader.AbnAmro.Tab.Tests/Tests/AbnAmroTabReaderHelpers/BankTransferLineParserTests.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using CashFlower.BankTransferReader.AbnAmro.Tab.AbnAmroTabReaderHelpers;
 using CashFlower.Test.Shared;
 using NUnit.Framework;
 
@@ -11,56 +13,64 @@ namespace CashFlower.BankTransferReader.AbnAmro.Tab.Tests.Tests.AbnAmroTabReader
         [ExpectedCashFlowerException("CFE_ABN_001")]
         public void GivenFileWithWrongNumberOfTabs_ThenExceptionIsThrown()
         {
-            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\ExampleLine_WrongNumberOfTabs.TAB");
-            new AbnAmroTabReader(filename).GetBankTransfers();
+            BankTransferLineParser.Execute(
+                "480226345	480226345	EUR	20160307	85,00	31,49	20160307	-53,51	BEA   NR:108T6F   07.03.16/19.57 ALBERT HEIJN 1394 NORTHR,PAS464 ");
         }
 
         [Test]
         [ExpectedCashFlowerException("CFE_ABN_002")]
         public void GivenLineWithOtherCurrencyThanEuro_AnExceptionIsThrown()
         {
-            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\ExampleLine_WrongCurrency.TAB");
-            new AbnAmroTabReader(filename).GetBankTransfers();
+            BankTransferLineParser.Execute(
+                "480226345	DOL	20160307	85,00	31,49	20160307	-53,51	BEA   NR:108T6F   07.03.16/19.57 ALBERT HEIJN 1394 NORTHR,PAS464 ");
         }
 
         [Test]
         [ExpectedCashFlowerException("CFE_ABN_003")]
         public void GivenLineWithInvalidTransactionDate_AnExceptionIsThrown()
         {
-            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\ExampleLine_InvalidTransactionDate.TAB");
-            new AbnAmroTabReader(filename).GetBankTransfers();
+            BankTransferLineParser.Execute("480226345	EUR	20161307	85,00	31,49	20160307	-53,51	BEA   NR:108T6F   07.03.16/19.57 ALBERT HEIJN 1394 NORTHR,PAS464 ");
         }
 
         [Test]
         [ExpectedCashFlowerException("CFE_ABN_004")]
         public void GivenLineWithInvalidInitialBalance_AnExceptionIsThrown()
         {
-            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\ExampleLine_InvalidInitialBalance.TAB");
-            new AbnAmroTabReader(filename).GetBankTransfers();
+            BankTransferLineParser.Execute("480226345	EUR	20160307	85.00	31,49	20160307	-53,51	BEA   NR:108T6F   07.03.16/19.57 ALBERT HEIJN 1394 NORTHR,PAS464 ");
         }
 
         [Test]
         [ExpectedCashFlowerException("CFE_ABN_005")]
         public void GivenLineWithInvalidFinalBalance_AnExceptionIsThrown()
         {
-            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\ExampleLine_InvalidFinalBalance.TAB");
-            new AbnAmroTabReader(filename).GetBankTransfers();
+            BankTransferLineParser.Execute("480226345	EUR	20160307	85,00	3149	20160307	-53,51	BEA   NR:108T6F   07.03.16/19.57 ALBERT HEIJN 1394 NORTHR,PAS464 ");
         }
 
         [Test]
         [ExpectedCashFlowerException("CFE_ABN_006")]
         public void GivenLineWithInvalidInterestDate_AnExceptionIsThrown()
         {
-            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\ExampleLine_InvalidInterestDate.TAB");
-            new AbnAmroTabReader(filename).GetBankTransfers();
+            BankTransferLineParser.Execute("480226345	EUR	20160307	85,00	31,49	20161307	-53,51	BEA   NR:108T6F   07.03.16/19.57 ALBERT HEIJN 1394 NORTHR,PAS464 ");
         }
 
         [Test]
         [ExpectedCashFlowerException("CFE_ABN_007")]
         public void GivenLineWithInvalidAmount_ExceptionIsThrown()
         {
-            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\ExampleLine_InvalidAmount.TAB");
-            new AbnAmroTabReader(filename).GetBankTransfers();
+            BankTransferLineParser.Execute("480226345	EUR	20160307	85,00	31,49	20160307	-53a5,1	BEA   NR:108T6F   07.03.16/19.57 ALBERT HEIJN 1394 NORTHR,PAS464 ");
+        }
+
+        [Test]
+        public void GivenValidLine_ReturnsResult()
+        {
+            var bankTransferLine = BankTransferLineParser.Execute("480226345	EUR	20160307	85,00	31,49	20160307	-53,51	BEA   NR:108T6F   07.03.16/19.57 ALBERT HEIJN 1394 NORTHR,PAS464 ");
+            Assert.AreEqual("480226345", bankTransferLine.AccountNumber);
+            Assert.AreEqual(new DateTime(2016, 3, 7, 19, 57, 0), bankTransferLine.TransactionDate);
+            Assert.AreEqual(85, bankTransferLine.InitialBalance);
+            Assert.AreEqual(31.49, bankTransferLine.FinalBalance);
+            Assert.AreEqual(new DateTime(2016, 3, 7), bankTransferLine.InterestDate);
+            Assert.AreEqual((decimal)-53.51, bankTransferLine.Amount);
+            Assert.AreEqual("ALBERT HEIJN 1394 NORTHR", bankTransferLine.ContraAccountDescription);
         }
     }
 }
--- CashFlower/CashFlower.BankTransferReader.AbnAmro.Tab.Tests/Tests/AbnAmroTabReaderHelpers/ContraAcountDetailsExtracterTests.cs
+++ CashFlower/CashFlower.BankTransferReader.AbnAmro.Tab.Tests/Tests/AbnAmroTabReaderHelpers/ContraAcountDetailsExtracterTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
+using CashFlower.BankTransferReader.AbnAmro.Tab.AbnAmroTabReaderHelpers;
 using CashFlower.Test.Shared;
 using NUnit.Framework;
 
@@ -13,28 +12,23 @@ namespace CashFlower.BankTransferReader.AbnAmro.Tab.Tests.Tests.AbnAmroTabReader
         [ExpectedCashFlowerException("CFE_ABN_008")]
         public void GivenLineWithPaymentFromPointOfSaleTerminalWithoutEnoughCharacters_ExceptionIsThrown()
         {
-            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\ExampleLine_PointOfSaleTerminalDetailsWithoutEnoughCharacters.TAB");
-            new AbnAmroTabReader(filename).GetBankTransfers();
+            ContraAcountDetailsExtracter.Execute("BEA   NR:108T6F   07.03.16/19.57");
         }
 
         [Test]
         [ExpectedCashFlowerException("CFE_ABN_009")]
         public void GivenLineWithPaymentFromPointOfSaleTerminalWithInvalidDate_ExceptionIsThrown()
         {
-            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\ExampleLine_PointOfSaleTerminalDetailsWithInvalidDate.TAB");
-            new AbnAmroTabReader(filename).GetBankTransfers();
+            ContraAcountDetailsExtracter.Execute("BEA   NR:108T6F   07.03.2016/19.57 ALB");
         }
 
         [Test]
         public void GivenLineWithPaymentFromPointOfSaleTerminalWitJustEnoughCharacters_BanktransferLineIsReturned()
         {
-            var filename = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Recources\ExampleLine_PointOfSaleTerminalDetailsWithJustEnoughCharacters.TAB");
-            var result = new AbnAmroTabReader(filename).GetBankTransfers();
-            Assert.AreEqual(1, result.Count);
+            var result = ContraAcountDetailsExtracter.Execute("BEA   NR:108T6F   07.03.16/19.57 ");
 
-            var bankTransferLine = result.Single();
-            Assert.AreEqual(new DateTime(2016, 3, 7, 19, 57, 0), bankTransferLine.TransactionDate);
-            Assert.AreEqual("", bankTransferLine.ContraAccountDescription);
+            Assert.AreEqual(new DateTime(2016, 3, 7, 19, 57, 0),result.DateTimeStamp);
+            Assert.AreEqual("", result.ContraAccountName);
         }
     }
 }
